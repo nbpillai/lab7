ad6e3c145724c05e873221a65436256ae1b0d409
Nitya B Pillai <cs15lwi23aql@ieng6-201.ucsd.edu>
2023-02-27 18:26:42 -0800

    fixed bug

--- ListExamples.java
+++ ListExamples.java
@@ -36,11 +36,12 @@ class ListExamples {
     }
     while(index1 < list1.size()) {
       result.add(list1.get(index1));
-      index1 += 1;
+      
+	index1 += 1;
     }
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
-      index1 += 1;
+      index2 += 1;
     }
     return result;
   }
